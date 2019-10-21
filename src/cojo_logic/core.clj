0cfd2180192b0c81b71d1cd58634aa23f224c3f2
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-10-21 15:49:20 +0200

    Adds solution for columns

--- src/cojo_logic/core.clj
+++ src/cojo_logic/core.clj
@@ -193,6 +193,34 @@
   ;; but also all columns.
 
   ;; SOLUTION
+  (partition 9 (first
+                (let [hints [2 0 7 0 1 0 5 0 8
+                             0 0 0 6 7 8 0 0 0
+                             8 0 0 0 0 0 0 0 6
+                             0 7 0 9 0 6 0 5 0
+                             4 9 0 0 0 0 0 1 3
+                             0 3 0 4 0 1 0 2 0
+                             5 0 0 0 0 0 0 0 1
+                             0 0 0 2 9 4 0 0 0
+                             3 0 6 0 8 0 4 0 9]
+                      vars (repeatedly (count hints) lvar)
+                      rows (rows vars)
+                      cols (transpose rows)]
+                  (run 1 [q]
+                       (== q vars)
+                       (init vars hints)
+                       (everyg #(fd/in % (fd/domain 1 2 3 4 5 6 7 8 9)) vars)
+                       (everyg fd/distinct rows)
+                       (everyg fd/distinct cols)))))
+  ;; => ((2 4 7 3 1 9 5 6 8)
+  ;;     (9 1 5 6 7 8 2 3 4)
+  ;;     (8 2 1 5 3 7 9 4 6)
+  ;;     (1 7 8 9 4 6 3 5 2)
+  ;;     (4 9 2 8 6 5 7 1 3)
+  ;;     (6 3 9 4 5 1 8 2 7)
+  ;;     (5 8 4 7 2 3 6 9 1)
+  ;;     (7 6 3 2 9 4 1 8 5)
+  ;;     (3 5 6 1 8 2 4 7 9))
 
   ;; ASSIGNMENT
   ;; Extend the query to not only cause all rows and columns to have distinct
