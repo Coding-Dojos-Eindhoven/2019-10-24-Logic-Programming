ca3336f0aa1397e5047acf7dc3d3977d764f133f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-10-22 15:35:38 +0200

    Takes the minimum needed from `core` to solve the sudoku

--- src/cojo_logic/sudoku.clj
+++ src/cojo_logic/sudoku.clj
@@ -23,41 +23,14 @@
 (defn rows
   "Takes a one-dimensional array of values and converts it
    into a matrix (array of rows)."
-  [elems]
-  (->> elems
-       (partition 9)
-       (map vec)
-       (into [])))
-(comment
-  (rows sudoku)
-  ;; => [[2 0 7 0 1 0 5 0 8]
-  ;;     [0 0 0 6 7 8 0 0 0]
-  ;;     [8 0 0 0 0 0 0 0 6]
-  ;;     [0 7 0 9 0 6 0 5 0]
-  ;;     [4 9 0 0 0 0 0 1 3]
-  ;;     [0 3 0 4 0 1 0 2 0]
-  ;;     [5 0 0 0 0 0 0 0 1]
-  ;;     [0 0 0 2 9 4 0 0 0]
-  ;;     [3 0 6 0 8 0 4 0 9]]
-  )
+  [elements]
+  (mapv vec (partition 9 elements)))
 
 (defn transpose
   "Transposes a matrix. Represents the matrix as a list of columns
    instead of rows."
   [matrix]
-  (into [] (apply map vector matrix)))
-(comment
-  (transpose (rows sudoku))
-  ;; => [[2 0 8 0 4 0 5 0 3]
-  ;;     [0 0 0 7 9 3 0 0 0]
-  ;;     [7 0 0 0 0 0 0 0 6]
-  ;;     [0 6 0 9 0 4 0 2 0]
-  ;;     [1 7 0 0 0 0 0 9 8]
-  ;;     [0 8 0 6 0 1 0 4 0]
-  ;;     [5 0 0 0 0 0 0 0 4]
-  ;;     [0 0 0 5 1 2 0 0 0]
-  ;;     [8 0 6 0 3 0 1 0 9]]
-  )
+  (apply mapv vector matrix))
 
 (defn get-square
   "Returns a seq of values of a 3x3 square submatrix at given coordinates."
@@ -65,10 +38,6 @@
   (for [x (range x (+ x 3))
         y (range y (+ y 3))]
     (get-in matrix [x y])))
-(comment
-  (get-square (rows (range 81)) 0 0)
-  ;; => (0 1 2 9 10 11 18 19 20)
-  )
 
 (defn squares
   "Returns a list of all squares of the sudoku."
@@ -76,19 +45,6 @@
   (for [x (range 0 9 3)
         y (range 0 9 3)]
     (get-square matrix x y)))
-
-(comment
-  (squares (rows sudoku))
-  ;; => ((2 0 7 0 0 0 8 0 0)
-  ;;     (0 1 0 6 7 8 0 0 0)
-  ;;     (5 0 8 0 0 0 0 0 6)
-  ;;     (0 7 0 4 9 0 0 3 0)
-  ;;     (9 0 6 0 0 0 4 0 1)
-  ;;     (0 5 0 0 1 3 0 2 0)
-  ;;     (5 0 0 0 0 0 3 0 6)
-  ;;     (0 0 0 2 9 4 0 8 0)
-  ;;     (0 0 1 0 0 0 4 0 9))
-  )
 
 ;;
 ;; So far everything was plain functional programming. Now we start
